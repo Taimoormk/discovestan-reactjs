f628b5938e89d3aa5dee271ddfa0009d720708e3
Taimoor Khan <taimoor.m.k@live.com>
2018-03-30 23:22:18 +0500

    app optimisations

--- src/js/components/Hero.jsx
+++ src/js/components/Hero.jsx
@@ -6,12 +6,22 @@ import { connect } from 'react-redux';
 // ########## Import Screens Here ##########
 
 // ########## Import Components Here ##########
+import { appData } from './appData';
 
 class Hero extends Component {
   render() {
     return(
       <section id="hero">
-        <div className="hero-background"></div>
+        <div className="hero-background">
+          <div className="content-container">
+            <h1 className="sectionH1">{appData[0].sectionH1}</h1>
+            <h2 className="sectionH2">{appData[0].sectionH2}</h2>
+            <h3 className="sectionH3">{appData[0].sectionH3}</h3>
+            <h4 className="sectionH4">{appData[0].sectionH4}</h4>
+            <img className="app-store-links" src="./images/apple.png" alt="Discovestan Apple Store link" />
+            <img className="app-store-links" src="./images/google.png" alt="Discovestan Google Play link" />
+          </div>
+        </div>
       </section>
     );
   }
---
+++ src/js/components/appData.js
@@ -0,0 +1,10 @@
+export const appData = [
+  {
+    id: 1,
+    sectionName: 'hero',
+    sectionH1: 'Discovestan',
+    sectionH2: 'Discover Pakistan with',
+    sectionH3: 'us as we bring the',
+    sectionH4: 'best stories for you...'
+  }
+]
