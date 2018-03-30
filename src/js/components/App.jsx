6f503c5e7f9fdd1d48d69d295fea21405e3bd548
Taimoor Khan <taimoor.m.k@live.com>
2018-03-30 22:19:07 +0500

    app barebones deployed

---
+++ src/js/components/App.jsx
@@ -0,0 +1,18 @@
+// ########## Import Dependencies Here ##########
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+// ########## Import Screens Here ##########
+
+// ########## Import Components Here ##########
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
+export default App;
